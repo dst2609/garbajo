38ed9587025db43492127b9963adb233778eba9c
Devarsh Thaker <devarsht@gmail.com>
2023-10-24 19:26:04 -0700

    search added

--- myArrayList.cpp
+++ myArrayList.cpp
@@ -15,16 +15,31 @@ template <class elemType> arrayListType<elemType>::~arrayListType() {
 
 template <class elemType>
 void arrayListType<elemType>::insertEnd(const elemType &insertItem) {
+    if (length == maxSize){
+        cout << "FULL! \n" << endl;
+        return;
+    }
+    list[length++] = insertItem;
  
 }
 
 template <class elemType>
 int arrayListType<elemType>::seqSearch(const elemType &item) const {
-
+    for (int i = 0; i < length; i++) {
+        if (list[i] == item) return i;
+    }
+    return -1;
 }
 
 template <class elemType>
 int arrayListType<elemType>::binarySearch(const elemType &item, int start, int end) const {
+    if (end == -1) end = length;    //end will be 1 even for 1 length of element
+    if (start >= end) return -1;
+    int mid = start + (end-start)/2;
+    if (list[mid] == item) return mid;
+    if (list[mid] > item) end = mid;
+    else start = mid+1; //[start, end) end not included so mid+1
+    return binarySearch(item, start, end);
 
 }
 
@@ -55,7 +70,9 @@ int main() {
         mylist.insertEnd(i*100 + 17);
     }
 
-    int to_find = 100000017;
+    // int to_find = 100000017;
+    int to_find = 199999917;
+    // int to_find = 117;
     auto start = high_resolution_clock::now();
     int ind = mylist.seqSearch(to_find);
     duration<double, micro> duration = high_resolution_clock::now() - start;
